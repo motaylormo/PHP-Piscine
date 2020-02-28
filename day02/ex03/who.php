06f70ee8464252807f1b636183012f05583e106c
Molly Taylor <42254557+motaylormo@users.noreply.github.com>
2020-02-28 12:20:27 -0800

    Update who.php

--- day02/ex03/who.php
+++ day02/ex03/who.php
@@ -8,16 +8,16 @@ date_default_timezone_set('America/Los_Angeles');
 		header "6.3 UTMPX File" - https://www.ma.rhul.ac.uk/static/techrep/2015/RHUL-MA-2015-8.pdf
 	format codes - https://www.php.net/manual/en/function.pack.php
 */
-$record_format =//					BYTES	VALUE
-"a256".	"user/"//		(string)	256		Username
-."a4".	"id/"//			(string)	4		Terminal indentifier
-."a32".	"line/"//		(string)	32		Terminal (tty name or line)
-."i".	"pid/"//					4		Process identifier (PID)
-."i".	"type/"//					4*		Type of login
-."i".	"timestamp/"//				4		Timestamp
-."i".	"microseconds/"//			4		Microseconds
-."a256"."host/"//		(string)	256		Hostname
-."x64".	"padding"//		(16 ints)	64		Padding for future fields
+$record_format =//				BYTES	VALUE
+"a256".	"user/"//		(string)	256	Username
+."a4".	"id/"//			(string)	4	Terminal indentifier
+."a32".	"line/"//		(string)	32	Terminal (tty name or line)
+."i".	"pid/"//				4	Process identifier (PID)
+."i".	"type/"//				4*	Type of login
+."i".	"timestamp/"//				4	Timestamp
+."i".	"microseconds/"//			4	Microseconds
+."a256"."host/"//		(string)	256	Hostname
+."x64".	"padding"//		(16 ints)	64	Padding for future fields
 ;
 $record_length = 628;
 
